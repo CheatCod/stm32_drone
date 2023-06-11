9ef5e94231a29300498912038b02b48036fe4622
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2023-06-11 19:31:08 -0400

    we have fan spin!

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::adc::Adc;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{Level, Output, Speed, Input, Pull};
 use embassy_stm32::pwm::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::pwm::{self, Channel};
 use embassy_stm32::time::{hz, khz};
@@ -19,6 +20,9 @@ async fn main(_spawner: Spawner) {
     let mut p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
+    let button = Input::new(p.PC13, Pull::None);
+    let mut button = ExtiInput::new(button, p.EXTI13);
+
     let mut led = Output::new(p.PA5, Level::High, Speed::Low);
     let mut delay = Delay;
     let mut adc = Adc::new(p.ADC1, &mut delay);
@@ -27,11 +31,21 @@ async fn main(_spawner: Spawner) {
     let mut pwm = SimplePwm::new(p.TIM2, None, Some(servo_pin), None, None, hz(50));
     let max_duty = pwm.get_max_duty();
 
-    pwm.enable(Channel::Ch2);
+    let mut button_last_pressed = button.is_low();
     loop {
+        let button_pressed = button.is_low();
+
+        if(button_last_pressed && !button_pressed) {
+            pwm.disable(Channel::Ch2);
+        }
+        if(!button_last_pressed && button_pressed) {
+            pwm.enable(Channel::Ch2);
+        }
+        button_last_pressed = button_pressed;
+
         led.set_high();
         let sample = adc.read(&mut p.PA0);
-        let percent = sample as f32 / 4096.0;
+        let percent = sample as f32 / 4096.0 / 2.0;
         // servo is 50hz, 20ms period, 1ms to 2ms duty cycle
         let period_duration = max_duty as f32 / 20.0;
         let duty = ((1.0 + percent) * period_duration) as u16;
@@ -42,6 +56,7 @@ async fn main(_spawner: Spawner) {
         );
 
         info!("outputting {}/{} ({}%)", duty, max_duty, percent * 100.0);
+        info!("input: {}", button_pressed);
         led.set_low();
         Timer::after(Duration::from_millis(10)).await;
     }
